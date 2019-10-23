878eb86ddfd627e11040eb061acc83a9b5979934
meteoaza <meteoaza@gmail.com>
2019-10-23 11:26:49 +0000

    23.10.2019-2.1-telg

---
+++ mybot_messages.py
@@ -0,0 +1,8 @@
+start_text = 'KANmeteo_bot greetings you! And ready for your command\nYou can use commands listed below'\
+    '\n\n/help  -- for get help\n/sensors -- to get list of sensors\n/value sensor -- to get sensor value\n'\
+    '/status sensor -- to get sensor status\n/errors -- to get errors recieve setting\n/errors on -- to set'\
+    'errors notification on\n/errors off -- to set errors notification off\n'
+help_text = 'Commands to interract with KANbot'\
+    '\n\n/help  -- for get help\n/sensors -- to get list of sensors\n/value sensor -- to get sensor value\n/status'\
+    'sensor -- to get sensor status\n/errors -- to get errors recieve setting\n/errors on -- to set errors notification'\
+     ' on\n/errors off -- to set errors notification off\n'
